7fe82181a7cb4d040e72ef54dfd18c5b488c7fc4
artChistyakov <132196101+artChistyakov@users.noreply.github.com>
2026-04-03 20:14:05 +0300

    Add lightbox feature to display images
    Implement lightbox functionality for image display.

---
+++ Lightbox_FINISH/js.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+	$(".item").on('click', function(){
+		let src = $(this).children("img").attr("src");
+		$(".overlay img").attr("src", src);
+		$(".overlay").addClass("open");
+	});
+	$(".overlay").on('click', function(){
+		$(".overlay").removeClass("open");
+	});
+});
+// $(document).ready(function () {
+//   $(".item").on('click', function () {
+//     let src = $(this).children("img").attr("src");
+//     $(".overlay img").attr("src", src);
+//     $(".overlay").addClass("open");
+//   });
+
+//   $(".close").on('click', function () {
+//     $(".overlay").removeClass("open");
+//   });
+// });
